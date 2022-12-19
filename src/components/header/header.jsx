46b0134de0754b31831360153833f5b9cc6ed3a5
DiyorbekkAbullayev <github1805@gmail.com>
2022-12-19 17:46:48 +0500

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import "../../App.css";
+import style from "./header.module.scss";
 const Header = () => {
-    return (
-        <header>
+  return (
+    <header className={style.header}>
+      <div className="container">
+        <nav className={style.nav}>
+          <img className={style.header__logo} alt="logo" />
+          <div className={style.header__wrapper}>
+            <ul className={style.header__list}>
+                <li className={style.header__item}>Home</li>
+                <li className={style.header__item}>About us</li>
+                <li className={style.header__item}>Features</li>
+                <li className={style.header__item}>Pricing</li>
+                <li className={style.header__item}>FAQ</li>
+                <li className={style.header__item}>Blog</li>
+            </ul>
             
-        </header>
-    );
-}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
