cfbebc84362b19843d8f0cbc6418d97009dc4594
DiyorbekkAbullayev <github1805@gmail.com>
2022-12-19 18:19:57 +0500

    headerr

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../App.css";
+import Button from "../library/Button";
 import style from "./header.module.scss";
 const Header = () => {
   return (
@@ -9,15 +10,14 @@ const Header = () => {
           <img className={style.header__logo} alt="logo" />
           <div className={style.header__wrapper}>
             <ul className={style.header__list}>
-                <li className={style.header__item}>Home</li>
-                <li className={style.header__item}>About us</li>
-                <li className={style.header__item}>Features</li>
-                <li className={style.header__item}>Pricing</li>
-                <li className={style.header__item}>FAQ</li>
-                <li className={style.header__item}>Blog</li>
-                <li className={style.header__item}>Blog</li>
+              <li className={style.header__item}>Home</li>
+              <li className={style.header__item}>About us</li>
+              <li className={style.header__item}>Features</li>
+              <li className={style.header__item}>Pricing</li>
+              <li className={style.header__item}>FAQ</li>
+              <li className={style.header__item}>Blog</li>
             </ul>
-
+            <Button className={style.btn}>contact us</Button>
           </div>
         </nav>
       </div>
